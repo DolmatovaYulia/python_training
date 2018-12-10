a6c0721b29e669015e4f1d5c6838e7d507c19d93
DolmatovaYulia <Yad201@yandex.ru>
2018-12-10 10:50:24 +0700

    Сценарий для создания нового контакта (test_add_contact)

---
+++ test_add_contact.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+
+    def test_add_contact(self):
+        driver = self.driver
+        driver.get("http://addressbook/")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_id("LoginForm").submit()
+        driver.find_element_by_link_text("add new").click()
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys("q")
+        driver.find_element_by_name("middlename").click()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys("w")
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys("e")
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("nickname").send_keys("qwerty")
+        driver.find_element_by_name("title").click()
+        driver.find_element_by_name("title").clear()
+        driver.find_element_by_name("title").send_keys("qwerty")
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys("qwerty")
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("address").send_keys("qwe")
+        driver.find_element_by_name("theform").click()
+        driver.find_element_by_name("home").click()
+        driver.find_element_by_name("home").clear()
+        driver.find_element_by_name("home").send_keys("123456")
+        driver.find_element_by_name("mobile").clear()
+        driver.find_element_by_name("mobile").send_keys("1234567890")
+        driver.find_element_by_name("theform").click()
+        driver.find_element_by_name("work").click()
+        driver.find_element_by_name("work").clear()
+        driver.find_element_by_name("work").send_keys("654321")
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("qwerty@qwe.rty")
+        driver.find_element_by_name("email2").click()
+        driver.find_element_by_name("email2").clear()
+        driver.find_element_by_name("email2").send_keys("qwerty")
+        driver.find_element_by_name("bday").click()
+        Select(driver.find_element_by_name("bday")).select_by_visible_text("1")
+        driver.find_element_by_name("bday").click()
+        driver.find_element_by_name("bmonth").click()
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("January")
+        driver.find_element_by_name("bmonth").click()
+        driver.find_element_by_name("byear").click()
+        driver.find_element_by_name("byear").clear()
+        driver.find_element_by_name("byear").send_keys("1999")
+        driver.find_element_by_name("aday").click()
+        Select(driver.find_element_by_name("aday")).select_by_visible_text("4")
+        driver.find_element_by_name("aday").click()
+        driver.find_element_by_name("amonth").click()
+        Select(driver.find_element_by_name("amonth")).select_by_visible_text("March")
+        driver.find_element_by_name("amonth").click()
+        driver.find_element_by_name("ayear").click()
+        driver.find_element_by_name("ayear").clear()
+        driver.find_element_by_name("ayear").send_keys("2000")
+        driver.find_element_by_name("address2").click()
+        driver.find_element_by_name("address2").clear()
+        driver.find_element_by_name("address2").send_keys("qwerty")
+        driver.find_element_by_name("phone2").click()
+        driver.find_element_by_name("phone2").clear()
+        driver.find_element_by_name("phone2").send_keys("qwewqe")
+        driver.find_element_by_name("notes").click()
+        driver.find_element_by_name("notes").clear()
+        driver.find_element_by_name("notes").send_keys("zxc")
+        driver.find_element_by_xpath("(//input[@name='submit'])").click()
+        driver.find_element_by_link_text("home").click()
+        driver.find_element_by_link_text("Logout").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
