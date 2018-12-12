64a90f86d5fb10eba3a1fb32a3f121abc6b7e4de
DolmatovaYulia <Yad201@yandex.ru>
2018-12-12 21:01:51 +0700

    Выделение фикстуры в отдельный класс в файле для добавления контакта (test_add_contact, application_for_contact)

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,176 +1,30 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 from contact import Contact
+from application_for_contact import Application
 
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-
-    def Logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
-
-    def Return_to_homepage(self, driver):
-        driver.find_element_by_link_text("home").click()
-
-    def Create_new_contact(self, driver, contact):
-        self.Add_new(driver)
-        # Add first name
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.firstname)
-        # Add middle name
-        driver.find_element_by_name("middlename").click()
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(contact.middlename)
-        # Add last name
-        driver.find_element_by_name("lastname").click()
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        # Add nickname
-        driver.find_element_by_name("nickname").click()
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(contact.nickname)
-        # Add photo
-        driver.find_element_by_name("photo").clear()
-        # Add title
-        driver.find_element_by_name("title").click()
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys(contact.title)
-        # Add company
-        driver.find_element_by_name("company").click()
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys(contact.company)
-        # Add address
-        driver.find_element_by_name("address").click()
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys(contact.address)
-        # Add home telephone
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(contact.home)
-        # Add mobile telephone
-        driver.find_element_by_name("mobile").click()
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys(contact.mobile)
-        # Add work telephone
-        driver.find_element_by_name("work").click()
-        driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys(contact.work)
-        # Add fax
-        driver.find_element_by_name("fax").click()
-        # Add email 1
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(contact.email)
-        # Add email 2
-        driver.find_element_by_name("email2").click()
-        driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys(contact.email2)
-        # Add email 3
-        driver.find_element_by_name("email3").click()
-        driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys(contact.email3)
-        # Add homepage
-        driver.find_element_by_name("homepage").click()
-        driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys(contact.homepage)
-        # Add bday
-        driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        driver.find_element_by_name("bday").click()
-        # Add bmonth
-        driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        driver.find_element_by_name("bmonth").click()
-        # Add byear
-        driver.find_element_by_name("byear").click()
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys(contact.byear)
-        # Add aday
-        driver.find_element_by_name("aday").click()
-        Select(driver.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        driver.find_element_by_name("aday").click()
-        # Add amonth
-        driver.find_element_by_name("amonth").click()
-        Select(driver.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        driver.find_element_by_name("amonth").click()
-        # Add ayear
-        driver.find_element_by_name("ayear").click()
-        driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys(contact.ayear)
-        # Add secondary address
-        driver.find_element_by_name("address2").click()
-        driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys(contact.address2)
-        # Add secondary telephone
-        driver.find_element_by_name("phone2").click()
-        driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys(contact.phone2)
-        # Add notes
-        driver.find_element_by_name("notes").click()
-        driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys(contact.notes)
-        # Submit new contact
-        driver.find_element_by_xpath("(//input[@name='submit'])").click()
-        self.Return_to_homepage(driver)
-
-    def Add_new(self, driver):
-        # Create contact
-        driver.find_element_by_link_text("add new").click()
-
-    def Login(self, driver, user_name, password):
-        self.Open_homepage(driver)
-        # Login
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(user_name)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_id("LoginForm").submit()
-
-    def Open_homepage(self, driver):
-        # Open homepage
-        driver.get("http://addressbook/")
+        self.app = Application()
 
     def test_add_contact(self):
-        driver = self.driver
-        self.Login(driver, user_name="admin", password="secret")
-        self.Create_new_contact(driver, Contact(firstname="q", middlename="w", lastname="e", nickname="qwerty", photo="", title="qwerty",
-                                                company="qwerty", address="qwe", home="123456", mobile="1234567890", work="654321", fax="",
-                                                email="qwerty@qwe.rty", email2="qwerty", email3="qwerty1@qwe.rty", homepage="none", bday="1",
-                                                bmonth="January", byear="1999", aday="4", amonth="March", ayear="2000", address2="qwerty", phone2="qwewqe", notes="zxc"))
-        self.Logout(driver)
+        self.app.Login(user_name="admin", password="secret")
+        self.app.Create_new_contact(Contact(firstname="q", middlename="w", lastname="e", nickname="qwerty", photo="", title="qwerty",
+                                        company="qwerty", address="qwe", home="123456", mobile="1234567890", work="654321", fax="",
+                                        email="qwerty@qwe.rty", email2="qwerty", email3="qwerty1@qwe.rty", homepage="none", bday="1",
+                                        bmonth="January", byear="1999", aday="4", amonth="March", ayear="2000", address2="qwerty", phone2="qwewqe", notes="zxc"))
+        self.app.Logout()
 
     def test_add_empty_contact(self):
-        driver = self.driver
-        self.Login(driver, user_name="admin", password="secret")
-        self.Create_new_contact(driver, Contact(firstname="", middlename="", lastname="", nickname="", photo="",title="", company="",address="",
-                                                home="", mobile="", work="", fax="",email="", email2="", email3="", homepage="",
-                                                bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
-        self.Logout(driver)
+        self.app.Login(user_name="admin", password="secret")
+        self.app.Create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", photo="",title="", company="",address="",
+                                        home="", mobile="", work="", fax="",email="", email2="", email3="", homepage="",
+                                        bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
+        self.app.Logout()
 
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
     def tearDown(self):
-        self.driver.quit()
+        self.app.Destroy()
 
 
 if __name__ == "__main__":
