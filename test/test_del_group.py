9f02c6567ac5697b1c18ba429cb5e6b7d8004887
DolmatovaYulia <Yad201@yandex.ru>
2018-12-18 20:36:03 +0700

    Изменение

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,6 +3,6 @@ from model.group import Group
 
 def test_del_group(app):
     if app.group.count() == 0:
-        app.group.Create(Group(user_name="test"))
+        app.group.Create(Group(group_name="test"))
     app.group.Delete_first_group()
 
