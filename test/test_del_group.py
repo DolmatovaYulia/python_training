4b85c9c8d41b4f4fddef2299218a140761ab0605
DolmatovaYulia <Yad201@yandex.ru>
2018-12-21 13:24:10 +0700

    Проверки в виде сравнения списов групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,5 +4,10 @@ from model.group import Group
 def test_del_group(app):
     if app.group.count() == 0:
         app.group.Create(Group(group_name="test"))
+    old_groups = app.group.get_group_list()
     app.group.Delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []
+    assert old_groups == new_groups
 
--- test/test_update_group.py
+++ test/test_update_group.py
@@ -4,24 +4,40 @@ from model.group import Group
 def test_update_group(app):
     if app.group.count() == 0:
         app.group.Create(Group(group_name="test"))
+    old_groups = app.group.get_group_list()
     app.group.Update_first_group(Group(group_name="group123", header="gr1", footer="gr2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_update_empty_group(app):
     if app.group.count() == 0:
         app.group.Create(Group(group_name="test"))
+    old_groups = app.group.get_group_list()
     app.group.Update_first_group(Group(group_name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_update_group_name(app):
     if app.group.count() == 0:
         app.group.Create(Group(group_name="test"))
-    app.group.Update_first_group(Group(group_name="new group"))
+    old_groups = app.group.get_group_list()
+    group = Group(group_name="new group")
+    group.id = old_groups[0].id
+    app.group.Update_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_update_group_header(app):
     if app.group.count() == 0:
         app.group.Create(Group(group_name="test"))
+    old_groups = app.group.get_group_list()
     app.group.Update_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
