4174e857f5e6238aa64f3ac2509e3c4f289ca7e2
DolmatovaYulia <Yad201@yandex.ru>
2018-12-24 16:48:53 +0700

    Параметризация тестов создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+# Генератор случайных строк
+def random_strint(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+# Генератор числовых строк
+def random_digits(prefix, maxlen):
+    symbols = string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(maxlen)])
+
+
+# Тестовые данные
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", photo="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="", notes="")] \
+           + [Contact(firstname=random_strint("name", 20), middlename=random_strint("middlename", 20), lastname=random_strint("lastname", 20), nickname=random_strint("nickname", 20), title=random_strint("title", 20), company=random_strint("company", 30), address=random_strint("address", 30), home=random_digits("", 6), mobile=random_digits("", 11), work=random_digits("", 6), fax=random_digits("fax", 20), email=random_strint("email", 20), email2=random_strint("email2", 20), email3=random_strint("email3", 20), homepage=random_strint("homepage", 20), address2=random_strint("address2", 20), phone2=random_digits("", 10), notes=random_strint("notes", 30))
+              for i in range(5)
+]
+
+
+# Передаются название параметра, куда должны передаваться тестовые данные, источник данных
+# и список с текстовым представлением данных
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="q", middlename="w", lastname="e", nickname="qwerty", photo="", title="qwerty", company="qwerty", address="qwe", home="123456", mobile="1234567890", work="654321", fax="", email="qwerty@qwe.rty", email2="qwerty", email3="qwerty1@qwe.rty", homepage="none", bday="1", bmonth="January", byear="1999", aday="4", amonth="March", ayear="2000", address2="qwerty", phone2="qwewqe", notes="zxc")
-    app.contact.Create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="", middlename="", lastname="", nickname="", photo="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="", notes="")
     app.contact.Create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-# Test method that takes a fixture as a parameter
-def test_add_group(app):
+# Генератор случайных строк
+def random_strint(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+# Тестовые данные
+testdata = [Group(group_name="", header="", footer="")] + [
+    Group(group_name=random_strint("name", 10), header=random_strint("header", 20), footer=random_strint("footer", 20))
+    for i in range(5)
+]
+
+
+# Передаются название параметра, куда должны передаваться тестовые данные, источник данных
+# и список с текстовым представлением данных
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(group_name="group", header="gr", footer="gr")
     app.group.Create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(group_name="", header="", footer="")
-    app.group.Create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-
-
