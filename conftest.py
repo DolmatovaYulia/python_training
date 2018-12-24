fa0cc21cce3d5eff399635b1f777361b693f7846
DolmatovaYulia <Yad201@yandex.ru>
2018-12-24 17:37:50 +0700

    Добавление опций командной строки, позволяющих указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -6,14 +6,17 @@ fixture = None
 
 
 @pytest.fixture
-def app():
+def app(request):
     # Initialization. Making fixtures
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        # функция создает объект класса Application
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_Login(user_name="admin", password="secret")
     return fixture
 
@@ -26,3 +29,9 @@ def stop(request):
     # How the fixture should be destroyed
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://addressbook/")
+
--- fixture/application.py
+++ fixture/application.py
@@ -6,17 +6,25 @@ from fixture.contact import ContactHelper
 
 # The class that contains all the helper methods.
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def Open_homepage(self):
         wd = self.wd
         if not (wd.current_url.endswith("http://addressbook/") and len(wd.find_elements_by_name("MassCB")) > 0):
-            wd.get("http://addressbook/")
+            wd.get(self.base_url)
 
     def Destroy(self):
         self.wd.quit()
