558e14e88cbbb07a6e0d7d2cb7a2ff624214f7a9
DolmatovaYulia <Yad201@yandex.ru>
2019-01-03 15:05:28 +0700

    Добавление изменений

--- test/test_del_all_contacts.py
+++ test/test_del_all_contacts.py
@@ -7,9 +7,10 @@ def test_del_all_contacts(app, db, check_ui):
     old_contacts = db.get_contact_list()
     app.contact.Delete_all_contacts()
     new_contacts = db.get_contact_list()
-    assert len(old_contacts) - len(old_contacts) == len(db.get_contact_list())
-    old_contacts.remove(old_contacts)
+    assert len(old_contacts) - len(old_contacts) == len(db.get_contact_list()) - len(db.get_contact_list())
+    old_contacts.clear()
+    new_contacts.clear()
     assert old_contacts == new_contacts
     if check_ui:
-        assert new_contacts == app.group.get_contact_list()
+        assert new_contacts == app.contact.get_contact_list()
 
